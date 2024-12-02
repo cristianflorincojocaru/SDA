8e4e72934b120e0363627752e52152dbb49d6564
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2024-12-02 20:24:31 +0200

    Create Kruskall.h

---
+++ Kruskall.h
@@ -0,0 +1,11 @@
+#ifndef _KRUSK_H_
+#define _KRUSK_H_
+
+#include <stdio.h>
+#include <iostream>
+#include "Graph.h" // Include structura grafului
+
+// Prototip pentru algoritmul lui Kruskal
+void kruskal(int &n, int &m, int L[20], graph *G);
+
+#endif
